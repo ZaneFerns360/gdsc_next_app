bb3341aa27efd6935f2c742c9d734bb827d10de9
Zane Fernandes <zane.ferns360@gmail.com>
2023-07-26 21:40:55 +0530

    added styles

--- app/page.jsx
+++ app/page.jsx
@@ -15,19 +15,27 @@ export default function Home() {
       </div>
 
       {/* Content Container 
-       we used the inset-0 class on the content container to position it to cover the entire parent container (main). 
+       I used the inset-0 class on the content container to position it to cover the entire parent container (main). 
        The flex class ensures it behaves as a flex container.*/}
       <div className="absolute inset-0 flex flex-col items-center justify-center">
         {/* Your content goes here */}
         <div className="container mx-auto p-8 text-white text-center relative top-[-250px]">
-          <h1 className="text-7xl font-bold blue_gradient">Welcome to <span className='blue_gradient'>GDSC</span></h1>
+          <h1 className="text-7xl font-bold blue_gradient">Welcome to <span className='google_gradient'>GDSC</span></h1>
           <p className="text-3xl font-bold orange_gradient pt-12">
-    Get ready to delve into the World of Developers
+          Get ready to delve into the World of Developers
           </p>
           {/* Add your other components here */}
+          <div className='flex-row'>
+        <div className='container'>
+          <div className='flex rounded-md shadow-md bg-black text-white'></div>
+        </div>
         </div>
       </div>
+      
+
+    </div>
     </main>
+    
   );
 }
 
